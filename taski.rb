336e96431b6470f60b67f7b6a5dbd37b7c4763fd
STUDENT <student@localhost.localdomain>
2015-02-24 08:52:40 +0530

    dis is my profile

---
+++ taski.rb
@@ -0,0 +1,15 @@
+def shout(val)
+puts val.upcase
+def add_array(val)
+sum=0
+val.each do |x|
+sum+=x
+end
+puts "sum=#{sum}"
+end def ammu
+shouts "dis will be explicitly divided"
+return
+puts "dis wnt execute"
+end
+shout("test") 
+
